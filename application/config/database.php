49b6af1ecec7c351f0d1034267af71b849c6a7bf
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-03-30 16:49:02 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,32 +1,26 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$servername = "localhost";
-$username = "azure";
-$password = "6#vWHD_$";
-$dbname = "db_web";
+$connectstr_dbhost = 'localhost';
+$connectstr_dbname = 'db_web';
+$connectstr_dbusername = 'azure';
+$connectstr_dbpassword = '6#vWHD_$';
 
-// Parsing connnection string
 foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
-    
-    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+  if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+    continue;
+  }
+
+  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "connection successful<br/>";
-}
-$conn->close();
+define('DB_NAME', $connectstr_dbname);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
+define('DB_HOST', $connectstr_dbhost);
 
 $active_group = 'default';
 $query_builder = true;
