e82c3308d99f0c880a43323129369a5b893e098b
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-03-30 16:45:24 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -27,5 +27,29 @@ if ($conn->connect_error) {
     echo "connection successful<br/>";
 }
 $conn->close();
-?>
+
+$active_group = 'default';
+$query_builder = true;
+
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => 'localhost',
+    'username' => 'azure',
+    'password' => '6#vWHD_$',
+    'database' => 'db_web',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => false,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => false,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => false,
+    'compress' => false,
+    'stricton' => false,
+    'failover' => array(),
+    'save_queries' => true
+);
 
