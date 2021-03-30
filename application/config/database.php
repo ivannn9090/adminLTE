e19ae5b0546a57aa05c9da23ea7027d4fd72e434
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-03-30 17:03:54 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -28,26 +28,7 @@ if ($conn->connect_error) {
 }
 $conn->close();
 
-$connectstr_dbhost = 'localhost';
-$connectstr_dbname = 'db_web';
-$connectstr_dbusername = 'azure';
-$connectstr_dbpassword = '6#vWHD_$';
 
-foreach ($_SERVER as $key => $value) {
-  if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-    continue;
-  }
-
-  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-define('DB_NAME', $connectstr_dbname);
-define('DB_USER', $connectstr_dbusername);
-define('DB_PASSWORD', $connectstr_dbpassword);
-define('DB_HOST', $connectstr_dbhost);
 
 $active_group = 'default';
 $query_builder = true;
