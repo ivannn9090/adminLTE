70b7ea762b407c00ed5ddc38f69be751568bdaa0
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-04-03 00:08:43 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -15,7 +15,33 @@ foreach ($_SERVER as $key => $value) {
   $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
+$active_group = 'default';
+$query_builder = true;
+
+
+
+   $db['default'] = array(
+    'dsn'    => '',
+
+
 define('DB_NAME', $connectstr_dbname);
 define('DB_USER', $connectstr_dbusername);
 define('DB_PASSWORD', $connectstr_dbpassword);
 define('DB_HOST', $connectstr_dbhost);
+
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => false,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => false,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => false,
+    'compress' => false,
+    'stricton' => false,
+    'failover' => array(),
+    'save_queries' => true
+);
+
