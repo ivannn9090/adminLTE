4ecc1146225930f77e6345d41fd84d50e1b66cb8
ivannn9090 <64771719+ivannn9090@users.noreply.github.com>
2021-04-02 23:59:14 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -1,51 +1,21 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+$connectstr_dbhost = 'localhost';
+$connectstr_dbname = 'db_web';
+$connectstr_dbusername = 'azure';
+$connectstr_dbpassword = '6#vWHD_$';
 
-$servername = "localhost";
-$username = "azure";
-$password = "6#vWHD_$";
-$dbname = "db_web";
-
-// Parsing connnection string
 foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
-    
-    $servername = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $username = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+  if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+    continue;
+  }
+
+  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-
-
-
-
-
-$active_group = 'default';
-$query_builder = true;
-
-
-
-   $db['default'] = array(
-    'dsn'    => '',
-    'hostname' => 'localhost',
-    'username' => 'azure',
-    'password' => '6#vWHD_$',
-    'database' => 'db_web',
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => false,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => false,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => false,
-    'compress' => false,
-    'stricton' => false,
-    'failover' => array(),
-    'save_queries' => true
-);
+define('DB_NAME', $connectstr_dbname);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
+define('DB_HOST', $connectstr_dbhost);
